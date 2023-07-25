946fd660b5232a8a983c2cfbd35324d3a337654b
Peachess <annasamokaeva2020@gmail.com>
2023-07-25 22:28:46 +0300

    set the app structure

---
+++ loopstudios/src/app/core/core.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
